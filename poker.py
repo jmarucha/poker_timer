2a609cc4ad9fc6c8da922a1af0fad5fd24fc5a28
frozear <75194437+frozear@users.noreply.github.com>
2022-09-05 00:11:45 -0700

    Update poker.py
    Many changes
    - Most notably, added a prompt at start so user can input some parameters. Among those parameters are blind amounts at all levels, thereby eliminating the need for the .csv file.
    -added an environment variable to suppress the pygame community message
    -added a pause in the while loop to save on cpu loading
    - Player count no longer effects the blinds; player count now is only used to calculate the average stack size among remaining players

--- poker.py
+++ poker.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
-import pygame
 import csv
 import sys
+import time
 from datetime import datetime, timedelta
-
-INTERVAL =  timedelta(minutes = 20)
-BGCOLOR = (32,32,32)
-FONTCOLOR = (240, 240, 240)
-STOPPEDCOLOR = (240, 30, 30)
-
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
 
 def render_time(time):
-	return "%02d:%02d" % (time.seconds//60, round(time.seconds % 60))
+	return "%2d:%02d" % (time.seconds//60, round(time.seconds % 60))
 
-def render_frame(time, small_blind, players, timer_done = False):
-	big_blind = 2*small_blind
+def render_frame(time, big_blind, next_big_blind, players, timer_done):
+	small_blind = big_blind/2
+	next_small_blind = next_big_blind/2
 	screen.fill(BGCOLOR)
 
-	if timer_done:
+	if timer_done == True:
 		time_surface = small_font.render("TIMER DONE!", True, STOPPEDCOLOR)
+		blind_surface = small_font.render("Blinds: %d / %d" % (big_blind, small_blind), True, FONTCOLOR)
+		player_surface = very_small_font.render("Player Count: %d" % (players), True, FONTCOLOR)
+		stack_surface = very_small_font.render("Average Stack: %d" % (total_chips/players), True, FONTCOLOR)
+		screen.blit(time_surface,(60,200))
+		screen.blit(blind_surface,(60,450))
+		screen.blit(player_surface,(60,60))
+		screen.blit(stack_surface,(60,100))
+		
 	else:
 		time_surface = big_font.render(render_time(time), True, STOPPEDCOLOR if paused else FONTCOLOR)
-	blind_surface = small_font.render("%d / %d" % (big_blind, small_blind), True, FONTCOLOR)
-	player_surface = very_small_font.render("%d players tournament:" % (players), True, FONTCOLOR)
-	screen.blit(time_surface,(60,150))
-	screen.blit(blind_surface,(60,450))
-	screen.blit(player_surface,(60,60))
+		blind_surface = small_font.render("Blinds: %d / %d" % (big_blind, small_blind), True, FONTCOLOR)
+		player_surface = very_small_font.render("Player Count: %d" % (players), True, FONTCOLOR)
+		stack_surface = very_small_font.render("Average Stack: %d" % (total_chips/players), True, FONTCOLOR)
+		next_blind_surface = very_small_font.render("Next Blinds: %d / %d" % (next_big_blind, next_small_blind), True, FONTCOLOR)
+		screen.blit(time_surface,(60,150))
+		screen.blit(blind_surface,(60,450))
+		screen.blit(player_surface,(60,60))
+		screen.blit(stack_surface,(60,100))
+		screen.blit(next_blind_surface,(60,700))
+
 
 def pause():
 	global paused, time, start
@@ -35,26 +46,33 @@ def pause():
 	else:
 		# stop pause
 		paused = False
-		paused_for_time = datetime.now() - time
+		paused_for_time = datetime.now() - time_now
 		start = start + paused_for_time
 
 def timer_done(sound = True):
-	global alert_sound, start, blind_level
+	global alert_sound, start, blind_level, next_blind_level
 	if sound:
 		alert_sound.play()
 	start = datetime.now()
 	blind_level = blind_level + 1
+	next_blind_level = next_blind_level + 1
 
 
-blinds = {}
-with open('blinds.csv', 'r') as csvfile:
-	blinds_file = csv.reader(csvfile, delimiter=',', quotechar='|')
-	for row in blinds_file:
-		row = [int(i) for i in row if i]
-		player_count = row[0]
-		blinds_list = row[1:]
-		blinds[player_count] = blinds_list
+players = int(input('How Many Players? '))
+starting_stack = int(input("How many chips in each player's starting stack? "))
+blind_duration = int(input('How many minutes will each rounds be? '))
+blind_list = input("List Big Blinds in order with a space between like '20 40 60 ...' ")
+blind_level = 0
+next_blind_level = 1
+total_chips = (players * starting_stack)
+blinds = (blind_list.split(" "))
 
+start = datetime.now()
+paused = False
+INTERVAL =  timedelta(minutes = blind_duration)
+BGCOLOR = (32,32,32)
+FONTCOLOR = (240, 240, 240)
+STOPPEDCOLOR = (240, 30, 30)
 
 pygame.display.init()
 pygame.display.set_caption("Poker Timer")
@@ -68,24 +86,18 @@ big_font = pygame.font.SysFont('DejaVu Sans', 240)
 small_font = pygame.font.SysFont('DejaVu Sans', 120)
 very_small_font = pygame.font.SysFont('DejaVu Sans', 30)
 
-start = datetime.now()
-blind_level = 0
-players = 4
-paused = False
-
 while 1:
 	if not paused:
-		time = datetime.now()
+		time_now = datetime.now()
 
-	if (INTERVAL < time - start):
+	if (INTERVAL < time_now - start):
 		timer_done()
 
 	try:
-		render_frame(INTERVAL - (time - start), blinds[players][blind_level], players)
+		render_frame(INTERVAL - (time_now - start), int(blinds[(blind_level)]), int(blinds[(next_blind_level)]), players, False)	
+
 	except IndexError:
-		render_frame(0, blinds[players][-1], players, timer_done = True)
-
-
+		render_frame(0, int(blinds[(blind_level)]), 0, players, True)
 
 	for event in pygame.event.get():
 		if event.type == pygame.KEYDOWN:
@@ -98,16 +110,14 @@ while 1:
 			else:
 				# change number of players
 				if event.key == pygame.K_UP:
-					new_players = players + 1
+					players = players + 1
 				elif event.key == pygame.K_DOWN:
-					new_players = players - 1
+					if players > 1:
+						players = players - 1
 				else:
 					new_players = event.key - ord('0')
-
-				if new_players in blinds.keys():
-					players = new_players
-					print(players)
-
+	
 		if event.type == pygame.QUIT:
 			sys.exit()
 	pygame.display.flip()
+	time.sleep(0.2)
